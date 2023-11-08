53d0339faa9d8644a010f191e192c158fa989f09
Ana Rodrigues <anananacr@gmail.com>
2023-11-08 12:58:27 +0100

    chorie: clean

--- scripts/jungfrau/conversion/convert_pedestals.sh
+++ scripts/jungfrau/conversion/convert_pedestals.sh
@@ -12,7 +12,6 @@
 
 source /home/rodria/scripts/regae/env-regae/bin/activate
 
-#INP=231020_mica_c4_m1_001/ed_rot_step_001/231020_mica_c4_m1_001_001_stop_pedal
 INP=$1
 MODE=$2
 N=$(basename $INP)
@@ -21,17 +20,17 @@ OUTPUT=/asap3/fs-bmx/gpfs/regae/2023/data/$beamtime/processed/darks
 echo $N
 
 ## convert start dark
-ls /asap3/fs-bmx/gpfs/regae/2023/data/$beamtime/raw/${INP}_start_pedal*_*d0_f*.h5>pedal_d0.lst
+ls /asap3/fs-bmx/gpfs/regae/2023/data/$beamtime/raw/${INP}*_start_pedal*_*d0_f*.h5>pedal_d0.lst
 /home/rodria/software/om_dev_regae/om/bin_src/om_jungfrau_dark.py pedal_d0.lst ${OUTPUT}/pedal_d0_${N}_${MODE}_start.h5
 
-ls /asap3/fs-bmx/gpfs/regae/2023/data/$beamtime/raw/${INP}_start_pedal*_*d1_f*.h5>pedal_d1.lst
+ls /asap3/fs-bmx/gpfs/regae/2023/data/$beamtime/raw/${INP}*_start_pedal*_*d1_f*.h5>pedal_d1.lst
 /home/rodria/software/om_dev_regae/om/bin_src/om_jungfrau_dark.py pedal_d1.lst ${OUTPUT}/pedal_d1_${N}_${MODE}_start.h5
 
 ## convert stop dark
-ls /asap3/fs-bmx/gpfs/regae/2023/data/$beamtime/raw/${INP}_stop_pedal*_*d0_f*.h5>pedal_d0.lst
+ls /asap3/fs-bmx/gpfs/regae/2023/data/$beamtime/raw/${INP}*_stop_pedal*_*d0_f*.h5>pedal_d0.lst
 /home/rodria/software/om_dev_regae/om/bin_src/om_jungfrau_dark.py pedal_d0.lst ${OUTPUT}/pedal_d0_${N}_${MODE}_stop.h5
 
-ls /asap3/fs-bmx/gpfs/regae/2023/data/$beamtime/raw/${INP}_stop_pedal*_*d1_f*.h5>pedal_d1.lst
+ls /asap3/fs-bmx/gpfs/regae/2023/data/$beamtime/raw/${INP}*_stop_pedal*_*d1_f*.h5>pedal_d1.lst
 /home/rodria/software/om_dev_regae/om/bin_src/om_jungfrau_dark.py pedal_d1.lst ${OUTPUT}/pedal_d1_${N}_${MODE}_stop.h5
 
 ## average dark
