bf883eea7826db9225d96db19e8f040f176e92d8
Ana Rodrigues <anananacr@gmail.com>
2024-05-17 16:22:30 +0200

    feat: masking when assembling

--- scripts/jungfrau/conversion/save_assembled_images.py
+++ scripts/jungfrau/conversion/save_assembled_images.py
@@ -21,11 +21,20 @@ def main(raw_args=None):
         "-g", "--geom", type=str, action="store", help="crystfel geometry file"
     )
     parser.add_argument(
+        "-m", "--mask", type=str, action="store", help="mask file 0 bad pixels 1 good pixels"
+    )
+    parser.add_argument(
         "-o", "--output", type=str, action="store", help="hdf5 output path"
     )
     args = parser.parse_args(raw_args)
 
     geometry_txt = open(args.geom, "r").readlines()
+
+    # Open mask
+    with h5py.File(f"{args.mask}", "r") as f:
+        mask = np.array(f["/data/data"])
+
+
     geom_info = geometry.GeometryInformation(
             geometry_description=geometry_txt, geometry_format="crystfel"
         )
@@ -41,14 +50,14 @@ def main(raw_args=None):
     sub.call(cmd, shell=True)
 
     f = h5py.File(f"{args.input}_master.h5", "r")
-    size = len(f["/entry/data/data"])
-    #size = len(f["/data"])
+    #size = len(f["/entry/data/data"])
+    size = len(f["/data"])
     label = (args.input).split("/")[-1]
 
     for i in range(size):
         try:
-            data = np.array(f["/entry/data/data"][i])
-            #data = np.array(f["/data"][i])
+            #data = np.array(f["/entry/data/data"][i])
+            data = np.array(f["/data"][i])
             data[np.where(data <= 0)] = -1
         except OSError:
             print("skipped", i)
@@ -56,7 +65,7 @@ def main(raw_args=None):
         visual_data = np.zeros(
             (visual_img_shape[0], visual_img_shape[1]), dtype=np.int32
         )
-        visual_data =  data_visualize.visualize_data(data=data)
+        visual_data =  data_visualize.visualize_data(data=data*mask)
         visual_data[np.where(visual_data<= 0)] = -1
         output_filename=f"{args.output}/{label}_{i:06}.tif"
         Image.fromarray(visual_data).save(f"{output_filename}")
--- scripts/screening/save_assembled_images.py
+++ scripts/screening/save_assembled_images.py
@@ -6,6 +6,7 @@ import os
 import subprocess as sub
 from PIL import Image
 import fabio
+import pathlib
 
 
 def main(raw_args=None):
@@ -20,11 +21,20 @@ def main(raw_args=None):
         "-g", "--geom", type=str, action="store", help="crystfel geometry file"
     )
     parser.add_argument(
+        "-m", "--mask", type=str, action="store", help="mask file 0 bad pixels 1 good pixels"
+    )
+    parser.add_argument(
         "-o", "--output", type=str, action="store", help="hdf5 output path"
     )
     args = parser.parse_args(raw_args)
 
     geometry_txt = open(args.geom, "r").readlines()
+
+    # Open mask
+    with h5py.File(f"{args.mask}", "r") as f:
+        mask = np.array(f["/data/data"])
+
+
     geom_info = geometry.GeometryInformation(
             geometry_description=geometry_txt, geometry_format="crystfel"
         )
@@ -34,17 +44,18 @@ def main(raw_args=None):
 
     raw_folder = os.path.dirname(args.input)
     output_folder = args.output
+    path = pathlib.Path(output_folder)
+    path.mkdir(parents=True, exist_ok=True)
     cmd = f"cp {raw_folder}/info.txt {output_folder}"
     sub.call(cmd, shell=True)
 
     f = h5py.File(f"{args.input}_master.h5", "r")
-    size = len(f["data"])
-
+    size = len(f["/entry/data/data"])
     label = (args.input).split("/")[-1]
 
     for i in range(size):
         try:
-            data = np.array(f["data"][i])
+            data = np.array(f["/entry/data/data"][i])
             data[np.where(data <= 0)] = -1
         except OSError:
             print("skipped", i)
@@ -52,12 +63,9 @@ def main(raw_args=None):
         visual_data = np.zeros(
             (visual_img_shape[0], visual_img_shape[1]), dtype=np.int32
         )
-        visual_data =  data_visualize.visualize_data(data=data)
+        visual_data =  data_visualize.visualize_data(data=data*mask)
         visual_data[np.where(visual_data<= 0)] = -1
         output_filename=f"{args.output}/{label}_{i:06}.tif"
-        #output=fabio.cbfimage.CbfImage(data=visual_data)
-        #output.write(output_filename)
-        # cbf.write(f'{args.output}/{label}_{i:06}.cbf', visual_data)
         Image.fromarray(visual_data).save(f"{output_filename}")
 
     f.close()
